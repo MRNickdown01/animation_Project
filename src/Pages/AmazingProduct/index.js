5d843e5e6522aec74e83c313eb78aae85ca0ed33
Nilesh Dubey <dubeynick123@gmail.com>
2023-07-12 17:29:17 +0530

    accordion issue pending add heading product page

--- src/Pages/AmazingProduct/index.js
+++ src/Pages/AmazingProduct/index.js
@@ -1,10 +1,11 @@
 import { Container, Grid } from "@mui/material";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./index.css";
 import $ from "jquery";
 
 const AmazingProduct = () => {
   // var $ = require( "jquery" );
+  const [isShow, setIsShow] = useState(false);
   useEffect(() => {
     $(function () {
       $(".acc_ctrl").on("click", function (e) {
@@ -20,6 +21,10 @@ const AmazingProduct = () => {
     });
   }, []);
 
+  const handleChange = () => {
+    setIsShow(!isShow);
+  };
+
   return (
     <section className="product_bg_section">
       <Container maxWidth="lg">
@@ -28,51 +33,75 @@ const AmazingProduct = () => {
           <Grid item xs={5}>
             <div className="Product_demo_girl"></div>
           </Grid>
-          <Grid item xs={6}>
-            <div className="container">
-              <ul className="acc">
-                <li>
-                  <button className="acc_ctrl">
-                    <h2>Holden</h2>
-                  </button>
-                  <div className="acc_panel">
-                    <p>
-                      GM Holden Ltd, commonly known as Holden, is an Australian
-                      automaker that operates in Australasia and is
-                      headquartered in Port Melbourne, Victoria. The company was
-                      founded in 1856 as a saddlery manufacturer in South
-                      Australia.
-                    </p>
-                  </div>
-                </li>
-                <li>
-                  <button className="acc_ctrl">
-                    <h2>Ford</h2>
-                  </button>
-                  <div className="acc_panel">
-                    <p>
-                      The Ford Motor Company (commonly referred to as simply
-                      Ford) is an American multinational automaker headquartered
-                      in Dearborn, Michigan, a suburb of Detroit. It was founded
-                      by Henry Ford and incorporated on June 16, 1903.
-                    </p>
-                  </div>
-                </li>
-                <li>
-                  <button className="acc_ctrl">
-                    <h2>Toyota</h2>
-                  </button>
-                  <div className="acc_panel">
-                    <p>
-                      Toyota Motor Corporation is a Japanese automotive
-                      manufacturer which was founded by Kiichiro Toyoda in 1937
-                      as a spinoff from his father's company Toyota Industries,
-                      which is currently headquartered in Toyota, Aichi
-                      Prefecture, Japan.
-                    </p>
-                  </div>
-                </li>
-              </ul>
+          <Grid item xs={6} sx={{ padding: "0px" }}>
+            <div className="product_detail_div">
+              <div className="heading_title">
+                <h1 className="Manrope-ExBold">
+                  We Create
+                  <strong style={{ color: "#6d6875" }}>
+                    {" "}
+                    Amazing Digital{" "}
+                  </strong>
+                  Products
+                </h1>
+                <p className="Manrope-Regular">
+                  With the greatest performance improvement and massive new
+                  features, the Salerio template pushes the limits of what is
+                  possible with a Webflow platform.
+                </p>
+              </div>
+              <div className="container">
+                <ul className="acc">
+                  <li>
+                    <button className="acc_ctrl" onClick={handleChange}>
+                      <h2 className="Manrope-ExBold">Holden</h2>
+                    </button>
+                    {isShow && (
+                      <div className="acc_panel">
+                        <p>
+                          GM Holden Ltd, commonly known as Holden, is an
+                          Australian automaker that operates in Australasia and
+                          is headquartered in Port Melbourne, Victoria. The
+                          company was founded in 1856 as a saddlery manufacturer
+                          in South Australia.
+                        </p>
+                      </div>
+                    )}
+                  </li>
+                  <li>
+                    <button className="acc_ctrl" onClick={handleChange}>
+                      <h2 className="Manrope-ExBold">Ford</h2>
+                    </button>
+                    {isShow && (
+                      <div className="acc_panel">
+                        <p>
+                          The Ford Motor Company (commonly referred to as simply
+                          Ford) is an American multinational automaker
+                          headquartered in Dearborn, Michigan, a suburb of
+                          Detroit. It was founded by Henry Ford and incorporated
+                          on June 16, 1903.
+                        </p>
+                      </div>
+                    )}
+                  </li>
+                  <li>
+                    <button className="acc_ctrl" onClick={handleChange}>
+                      <h2 className="Manrope-ExBold">Toyota</h2>
+                    </button>
+                    {isShow && (
+                      <div className="acc_panel">
+                        <p>
+                          Toyota Motor Corporation is a Japanese automotive
+                          manufacturer which was founded by Kiichiro Toyoda in
+                          1937 as a spinoff from his father's company Toyota
+                          Industries, which is currently headquartered in
+                          Toyota, Aichi Prefecture, Japan.
+                        </p>
+                      </div>
+                    )}
+                  </li>
+                </ul>
+              </div>
             </div>
           </Grid>
         </Grid>
--- src/Pages/MainRoutePage/index.js
+++ src/Pages/MainRoutePage/index.js
@@ -3,8 +3,24 @@ import HeaderSection from "../HeaderSection";
 import AboutSection from "../AboutSection";
 import AmazingProduct from "../AmazingProduct";
 import ReviewSection from "../ReviewSection";
+import $ from "jquery";
+import { useEffect } from "react";
 
 const MainRoutePage = () => {
+  // useEffect(() => {
+  //   $(function () {
+  //     $(".acc_ctrl").on("click", function (e) {
+  //       e.preventDefault();
+  //       if ($(this).hasClass("active")) {
+  //         $(this).removeClass("active");
+  //         $(this).next().stop().slideUp(300);
+  //       } else {
+  //         $(this).addClass("active");
+  //         $(this).next().stop().slideDown(300);
+  //       }
+  //     });
+  //   });
+  // }, []);
   return (
     <section className="maine_route_page">
       <div className="header_route">
